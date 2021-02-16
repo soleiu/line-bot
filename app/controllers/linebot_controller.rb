483c098abda6591574d9e4fa406ff715a6ac2b82
hishinumamegumi <himamegu847@gmail.com>
2021-02-16 17:05:44 +0900

    Update linebot_controller.rb

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -8,8 +8,8 @@ class LinebotController < ApplicationController
     body = request.body.read
     signature = request.env['HTTP_X_LINE_SIGNATURE']
     unless client.validate_signature(body, signature)
-      pp "aaa"
       return head :bad_request
+      pp "aaa"
     end
     events = client.parse_events_from(body)
     events.each { |event|
