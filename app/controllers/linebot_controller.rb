bf5a473ad24e85faac87545f2da55c389b24b0b8
hishinumamegumi <himamegu847@gmail.com>
2021-02-16 15:56:12 +0900

    test

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -5,12 +5,12 @@ class LinebotController < ApplicationController
   require 'rexml/document'
 
   def callback
-    pp "aaa"
     body = request.body.read
     signature = request.env['HTTP_X_LINE_SIGNATURE']
     unless client.validate_signature(body, signature)
       return head :bad_request
     end
+    pp "aaa"
     events = client.parse_events_from(body)
     events.each { |event|
       case event
