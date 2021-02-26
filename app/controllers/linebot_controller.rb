2e00dec5ec91f1c4bf7ee7c6ad86436533dbecf1
hishinumamegumi <himamegu847@gmail.com>
2021-02-26 22:59:50 +0900

    test

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -62,8 +62,7 @@ class LinebotController < ApplicationController
               "私はゲンガーが好き！あなたは何が好き？？"
           when /.*(お腹空いた|おなか減った|おなか空いた).*/
             push =
-              "今日のご飯は何かなー？楽しみ(o^^o)\n今日はパスタの気分！！！"
-              "何食べるのー？私も一緒に食べたいな！"
+              "何食べるのー？"
           else
             per06to12 = doc.elements[xpath + 'info/rainfallchance/period[2]l'].text
             per12to18 = doc.elements[xpath + 'info/rainfallchance/period[3]l'].text
