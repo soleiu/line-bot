0808a9fe70bc3ca2f8ad6b7688bbf444d68b74cc
hishinumamegumi <himamegu847@gmail.com>
2021-03-09 23:47:07 +0900

    test

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -174,8 +174,8 @@ class LinebotController < ApplicationController
                   "雨に負けずファイト！！",
                   "雨だけどああたの明るさでみんなを元気にしてあげて(*^^)*"].sample
               push =
-                "今日の天気？\n今日は雨が降りそうだから傘があった方が安心だよ。\n　  6〜12時　#{per06to12}％\n　12〜18時　 #{per12to18}％\n　18〜24時　#{per18to24}％\n#{word}"
-                pp "aaa"
+                "今日の天気？\n今日は雨が降りそうだから傘があった方が安心だよ。\n　6〜12時　#{per06to12}％\n　12〜18時　 #{per12to18}％\n　18〜24時　#{per18to24}％\n#{word}"
+                pp "QQQ"
             else
               word =
                 ["天気もいいからウォーキングしてみるのはどう？(o^^o)",
