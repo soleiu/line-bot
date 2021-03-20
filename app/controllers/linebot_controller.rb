ec365d946a2868cb3d05adf4dd9f051444956b0a
hishinumamegumi <himamegu847@gmail.com>
2021-03-20 18:20:26 +0900

    test

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -231,7 +231,7 @@ class LinebotController < ApplicationController
             per06to12 = doc.elements[xpath + 'info/rainfallchance/period[2]l'].text
             per12to18 = doc.elements[xpath + 'info/rainfallchance/period[3]l'].text
             per18to24 = doc.elements[xpath + 'info/rainfallchance/period[4]l'].text
-            PP "aaa"
+            puts "aaa"
             if per06to12.to_i >= min_per || per12to18.to_i >= min_per || per18to24.to_i >= min_per
               word =
                 ["雨だけど元気出していこうね！",
